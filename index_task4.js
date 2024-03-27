3e309149a3c98267f031d5acbc25d2567764a829
adityasinghz <adityasingh246810@gmail.com>
2024-03-27 20:33:06 +0530

    task 3 commit

--- index_task4.js
+++ index_task4.js
@@ -4,16 +4,23 @@ async function monitorButtonClick() {
     const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
 
-    await page.goto('http://localhost:3000/');
+    await page.goto('https://www.makemytrip.com/');
     await page.evaluate(() => {
         const observer = new MutationObserver(mutations => {
+            //console.log("mutations ", mutations);
             mutations.forEach(mutation => {
+                console.log("mutation ", mutation, "mutation.addedNodes.length ", mutation.addedNodes.length);
                 if (mutation.type === 'childList' && mutation.addedNodes.length) {
                     mutation.addedNodes.forEach(node => {
                         if (node.nodeName == 'DIV') {
-                            handleButtonClick();
+                            handleSelect();
                         }
                     });
+                }
+                else if (mutation.type === 'childList' && mutation.removedNodes.length) {
+                    if (mutation.target.nodeName == 'DIV') {
+                        handlePopUpButton();
+                    }
                 }
             });
         });
@@ -23,7 +30,18 @@ async function monitorButtonClick() {
     });
 
     await page.exposeFunction('handleButtonClick', () => {
-        console.log('Hello Im Clicked!');
+        console.log('Hello Button is Clicked!');
+    });
+    await page.exposeFunction('handlePopUpButton', () => {
+        console.log("Pop-up has been closed");
+    });
+    await page.exposeFunction('handleSelect', async () => {
+        const cityNames = await page.evaluate(() => {
+            const cityNodes = document.querySelectorAll('.font14.appendBottom5.blackText');
+            const cities = Array.from(cityNodes).map(node => node.textContent.trim());
+            return cities;
+        });
+        console.log('City Names:', cityNames);
     });
     //await browser.close();
 }
